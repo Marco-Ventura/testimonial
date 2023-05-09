ef9782d19ef7e55f7d6650f159c310ab4e132879
Marco Ventura <130122725+Marco-Ventura@users.noreply.github.com>
2023-05-08 17:02:06 -0700

    add new images, add props to testimony component

--- src/components/testimony.js
+++ src/components/testimony.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../stylesheets/testimony.css'
 
-function TestimonyComponent(){
+function TestimonyComponent(props){
   return(
     <div className='testimony-container'>
-      <img className='testimony-picture'    src={require(`../images/testimonio1.jpg`)} alt='Testimonio 1'/>
+      <img className='testimony-picture' 
+      src={require(`../images/testimonio${props.picture}.jpeg`)} alt='Testimonio 1'/>
       <div className='testimony-text'>
        <p className='name'>
-        JOSHUA FROM INDIANA
-        </p>
+        {props.name} FROM {props.city}
+       </p>
 
       <p>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
+        {props.testimony}
       </p>
       </div>
 
